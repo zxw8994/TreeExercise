718e54f014f639e3783bf601d627e734f5588ab9
Zachary Wilken <zxw8994@rit.edu>
2018-05-02 13:44:44 -0400

    added base tree code

---
+++ TreeExercise/TreeExercise/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeExercise
+{
+
+
+    class Node
+    {
+        public int value;
+        public int depth;
+        public Node leftChild;
+        public Node rightChild;
+    }
+
+    class Tree
+    {
+
+        public Node insertNode(Node root,int val)
+        {
+
+            if(root == null)
+            {
+                root = new Node();
+                root.value = val;
+            }
+
+            return root;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Node root = null;
+
+            Tree tree = new Tree();
+
+
+
+        }
+    }
+}
